e3822026bb58f88fb39e05c0bf47ab0e37678950
guhongliang <guhongliang@xiaozhu.com>
2019-09-30 18:26:24 +0800

    add custom isolate

--- lib/ui/guide_page.dart
+++ lib/ui/guide_page.dart
@@ -1,3 +1,5 @@
+import 'dart:isolate';
+
 import 'package:bloc/bloc.dart';
 import 'package:fluro/fluro.dart';
 import 'package:flutter/material.dart';
@@ -35,7 +37,7 @@ class SimpleBlocDelegate extends BlocDelegate {
   }
 }
 
-void main() {
+void main() async {
   BlocSupervisor.delegate = SimpleBlocDelegate();
   final userRepository = UserRepository();
   guidePageWidget();
@@ -50,6 +52,37 @@ void main() {
       ),
     ),
   );
+  print('结果是${await asyncFibonacci(20)}');
+}
+
+Future<dynamic> asyncFibonacci(int n) async {
+  //创建一个ReceivePort，因为创建isolate所需的参数，必须要有SendPort，SendPort需要ReceivePort来创建
+  final response = ReceivePort();
+  //创建isolate,通过Isolate.spawn()函数
+  await Isolate.spawn(_isolate, response.sendPort);
+  //获取sendPort来发送消息
+  var sendPort = await response.first as SendPort;
+  //接受消息的ReceivePort
+  final answer = ReceivePort();
+  //发送数据
+  sendPort.send([n, answer.sendPort]);
+  return answer.first;
+}
+
+///创建isolate
+void _isolate(SendPort initialRepTo) {
+  final port = ReceivePort();
+  initialRepTo.send(port.sendPort);
+  port.listen((msg) {
+    //获取数据并分析
+    final data = msg[0] as int;
+    final send = msg[1] as SendPort;
+    send.send(syncFibonacci(data));
+  });
+}
+
+int syncFibonacci(int n) {
+  return n < 2 ? n : syncFibonacci(n - 2) + syncFibonacci(n - 1);
 }
 
 void guidePageWidget() {
